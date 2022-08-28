bc2a321c995400b362e94a7f095dd17dc87d97c5
Alexander Silvera <alexandersilvera@hotmail.com>
2022-08-28 16:26:46 -0300

    Arreglos en Settings

--- backend/settings.py
+++ backend/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
-DEBUG = os.environ.get('DEBUG', default=True, cast=bool)
+DEBUG = os.environ.get('DEBUG')
 
 
 ALLOWED_HOSTS = [
